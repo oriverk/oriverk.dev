077a94531333c540cfe0d086bf6633fcc38cbcf3
oriverk <oriveryk@gmail.com>
2021-11-30 21:09:59 +0900

    modify jsx option in tsconfig and app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,14 @@
+import { h, Fragment } from "preact"
 import Header from "./components/Header"
 import Main from './components/Main'
 import Footer from './components/Footer'
 
 export function App() {
   return (
-    <>
+    <Fragment>
       <Header />
       <Main />
       <Footer />
-    </>
+    </Fragment>
   )
 }
