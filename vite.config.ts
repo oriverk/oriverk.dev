694b02bfedb8367093cdb4d4b933a8745079d159
oriverk <oriveryk@gmail.com>
2022-05-23 16:12:50 +0900

    remove vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,13 +8,6 @@ export default defineConfig({
     alias: {},
   },
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'README.md', dest: "." },
-        { src: 'Resume.md', dest: "." }
-      ]
-    })
-  ],
-  assetsInclude: [/\.mdx?$/]
+    react()
+  ]
 })
